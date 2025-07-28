f9bc47832d11bc85823453214643231fd83f1219
Mukunda-Katel <mukundakatel@gmail.com>
2025-07-28 14:13:00 +0545

    Working on deplyment 6

--- wsgi.py
+++ wsgi.py
@@ -15,6 +15,3 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings')
 # Import Django WSGI application
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
-
-# For Gunicorn
-app = application
